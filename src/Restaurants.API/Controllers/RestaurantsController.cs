5ad03277fd717e3c48278ebb784bcd8fd336a928
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-20 13:49:51 +0530

    Restaurants Controller

---
+++ src/Restaurants.API/Controllers/RestaurantsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurants.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RestaurantsController : ControllerBase
+{
+    [HttpGet]
+    [AllowAnonymous]
+    public IActionResult GetAll()
+    {
+        var restaurants = await mediator.Send(query);
+
+        return Ok(restaurants);
+    }
+}
