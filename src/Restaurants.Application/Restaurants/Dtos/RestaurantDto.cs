2bae4261a60b342041e5b64b64b4cc1458033df2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-20 14:37:20 +0530

    Dtos

---
+++ src/Restaurants.Application/Restaurants/Dtos/RestaurantDto.cs
@@ -0,0 +1,25 @@
+﻿using Restaurants.Application.Dishes.Dtos;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Application.Restaurants.Dtos;
+
+public class RestaurantDto
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public string Category { get; set; } = string.Empty;
+
+    public bool HasDelivery { get; set; }
+
+    public string City { get; set; } = "Some City";
+
+    public string Street { get; set; } = "Some Street";
+
+    public string PostalCode { get; set; } = "10001";
+
+    public List<DishDto> Dishes { get; set; } = [];
+}
