af913702a7680e91c2c4689d570623785679f8e7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-20 11:12:50 +0530

    Migrations

--- src/Restaurants.Domain/Entities/Address.cs
+++ src/Restaurants.Domain/Entities/Address.cs
@@ -2,9 +2,9 @@
 
 public class Address
 {
-    public string? City { get; set; }
+    public string City { get; set; } = "Some City";
 
-    public string? Street { get; set; }
+    public string Street { get; set; } = "Some Street";
 
-    public string? PostalCode { get; set; }
+    public string PostalCode { get; set; } = "10001";
 }
