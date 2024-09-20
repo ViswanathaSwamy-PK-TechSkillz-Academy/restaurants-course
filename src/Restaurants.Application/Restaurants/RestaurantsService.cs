7306e00d2511081d35a8a0172af35142db27ab25
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-20 14:09:21 +0530

    Service

--- src/Restaurants.Application/Restaurants/RestaurantsService.cs
+++ src/Restaurants.Application/Restaurants/RestaurantsService.cs
@@ -4,7 +4,7 @@ using Restaurants.Domain.Repositories;
 
 namespace Restaurants.Application.Restaurants;
 
-internal class RestaurantsService(IRestaurantsRepository restaurantsRepository, ILogger<RestaurantsService> logger)
+internal class RestaurantsService(IRestaurantsRepository restaurantsRepository, ILogger<RestaurantsService> logger) : IRestaurantsService
 {
 
     public async Task<IEnumerable<Restaurant>> GetAll()
