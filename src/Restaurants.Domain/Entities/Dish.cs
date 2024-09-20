a548d1813eb11a6d6a1f69a9d93f127e4eb5efb4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-20 07:32:13 +0530

    Initial Version

---
+++ src/Restaurants.Domain/Entities/Dish.cs
@@ -0,0 +1,16 @@
+﻿namespace Restaurants.Domain.Entities;
+
+public class Dish
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public decimal Price { get; set; }
+
+    public int? KiloCalories { get; set; }
+
+    public int RestaurantId { get; set; }
+}
