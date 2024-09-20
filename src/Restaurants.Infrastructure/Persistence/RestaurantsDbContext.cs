e062da06c016f557231072d2f8fa7aff7422e3f6
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-20 10:46:39 +0530

    Code Refactoring

---
+++ src/Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastructure.Persistence;
+
+internal class RestaurantsDbContext : DbContext
+{
+    internal DbSet<Restaurant> Restaurants => Set<Restaurant>();
+
+    internal DbSet<Dish> Dishes => Set<Dish>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string connectionString = "Server=localhost;Database=RestaurantsDb;Trusted_Connection=True;";
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
