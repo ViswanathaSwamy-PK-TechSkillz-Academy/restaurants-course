f98c9c69a9f5e927108cae4ba804c7bf848bc4de
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-21 23:27:59 +0530

    Update ServiceCollectionExtension.cs

--- src/Restaurants.Application/Extensions/ServiceCollectionExtension.cs
+++ src/Restaurants.Application/Extensions/ServiceCollectionExtension.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Restaurants.Application.Restaurants;
-using Restaurants.Application.Restaurants.Dtos;
 using System.Reflection;
 
 namespace Restaurants.Application.Extensions;
@@ -10,14 +9,10 @@ public static class ServiceCollectionExtension
 
     public static void AddApplication(this IServiceCollection services)
     {
-        Assembly? applicationAssembly = typeof(ServiceCollectionExtensions).Assembly;
+        Assembly? applicationAssembly = Assembly.GetExecutingAssembly();
 
         services.AddScoped<IRestaurantsService, RestaurantsService>();
 
-        // Explicitly register the AutoMapper profile(s)
-        services.AddAutoMapper(cfg =>
-        {
-            cfg.AddProfile<RestaurantsProfile>(); // Explicitly add the RestaurantsProfile
-        }, Assembly.GetExecutingAssembly()); // Still scanning the assembly in case there are other profiles
+        services.AddAutoMapper(applicationAssembly);
     }
 }
