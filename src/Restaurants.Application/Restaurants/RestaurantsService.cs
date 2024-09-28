092699e6b577bfb8e0fa10c9c04e5e5adc3af4d6
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-28 15:34:09 +0530

    Update RestaurantsService.cs

--- src/Restaurants.Application/Restaurants/RestaurantsService.cs
+++ src/Restaurants.Application/Restaurants/RestaurantsService.cs
@@ -5,6 +5,8 @@ using Restaurants.Domain.Repositories;
 
 namespace Restaurants.Application.Restaurants;
 
+// Note: These is not needed for now as we are implementing the CreateRestaurantCommandHandler
+
 internal class RestaurantsService(IRestaurantsRepository restaurantsRepository,
     ILogger<RestaurantsService> logger, IMapper mapper) : IRestaurantsService
 {
@@ -19,18 +21,18 @@ internal class RestaurantsService(IRestaurantsRepository restaurantsRepository,
     //    return restaurantsDto!; // It will not be null. It might be empty collection.
     //}
 
-    public async Task<RestaurantDto?> GetById(int id)
-    {
-        logger.LogInformation("Getting restaurant by {id}", id);
+    //public async Task<RestaurantDto?> GetById(int id)
+    //{
+    //    logger.LogInformation("Getting restaurant by {id}", id);
 
-        var restaurant = await restaurantsRepository.GetByIdAsync(id);
+    //    var restaurant = await restaurantsRepository.GetByIdAsync(id);
 
-        var restaurantDto = mapper.Map<RestaurantDto?>(restaurant);
+    //    var restaurantDto = mapper.Map<RestaurantDto?>(restaurant);
 
-        return restaurantDto;
-    }
+    //    return restaurantDto;
+    //}
 
-    // Note: This is not needed for now as we are implementing the CreateRestaurantCommandHandler
+
     //public async Task<int> Create(CreateRestaurantDto createRestaurantDto)
     //{
     //    logger.LogInformation("Creating a new restaurant");
@@ -41,6 +43,15 @@ internal class RestaurantsService(IRestaurantsRepository restaurantsRepository,
 
     //    return id;
     //}
+    public Task<IEnumerable<RestaurantDto>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<RestaurantDto?> GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
 }
 
 
