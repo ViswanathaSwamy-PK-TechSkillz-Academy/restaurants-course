5bc41624701a7924541ff5387759f7f7a88a2e2d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-28 15:32:13 +0530

    Code Refactoring

--- src/Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetRestaurantByIdQuery.cs
+++ src/Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetRestaurantByIdQuery.cs
@@ -3,7 +3,7 @@ using Restaurants.Application.Restaurants.Dtos;
 
 namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
 
-public class GetRestaurantByIdQuery(int id) : IRequest<RestaurantDto>
+public class GetRestaurantByIdQuery(int id) : IRequest<RestaurantDto?>
 {
     public int Id { get; } = id;
 }
--- src/Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetRestaurantByIdQueryHandler.cs
+++ src/Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetRestaurantByIdQueryHandler.cs
@@ -7,9 +7,9 @@ using Restaurants.Domain.Repositories;
 namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
 
 public class GetRestaurantByIdQueryHandler(ILogger<GetRestaurantByIdQueryHandler> logger,
-    IRestaurantsRepository restaurantsRepository, IMapper mapper) : IRequestHandler<GetRestaurantByIdQuery, RestaurantDto>
+    IRestaurantsRepository restaurantsRepository, IMapper mapper) : IRequestHandler<GetRestaurantByIdQuery, RestaurantDto?>
 {
-    public async Task<RestaurantDto> Handle(GetRestaurantByIdQuery request, CancellationToken cancellationToken)
+    public async Task<RestaurantDto?> Handle(GetRestaurantByIdQuery request, CancellationToken cancellationToken)
     {
         logger.LogInformation("Getting restaurant by {id}", request.Id);
 
