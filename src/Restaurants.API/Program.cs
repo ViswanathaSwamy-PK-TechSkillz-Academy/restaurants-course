5e930ef00a671d11cd5de26b56134e9e8d9a0dc8
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-10-01 22:33:19 +0530

    Update Program.cs

--- src/Restaurants.API/Program.cs
+++ src/Restaurants.API/Program.cs
@@ -1,10 +1,13 @@
 using Restaurants.API.Extensions;
 using Restaurants.Infrastructure.Seeders;
+using Serilog;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddPresentation(builder.Configuration);  // TODO: Use Strongly Type Configuration Object
+
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
 
 WebApplication app = builder.Build();
 
