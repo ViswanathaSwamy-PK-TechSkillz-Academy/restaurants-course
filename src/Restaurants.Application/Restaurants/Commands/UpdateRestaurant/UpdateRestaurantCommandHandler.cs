2f56a1ab16f1c5d819ee6cd716915a3ce475b398
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-10-03 17:59:34 +0530

    Code Refactoring

--- src/Restaurants.Application/Restaurants/Commands/UpdateRestaurant/UpdateRestaurantCommandHandler.cs
+++ src/Restaurants.Application/Restaurants/Commands/UpdateRestaurant/UpdateRestaurantCommandHandler.cs
@@ -8,9 +8,9 @@ using Restaurants.Domain.Repositories;
 namespace Restaurants.Application.Restaurants.Commands.UpdateRestaurant;
 
 public class UpdateRestaurantCommandHandler(ILogger<UpdateRestaurantCommandHandler> logger,
-    IRestaurantsRepository restaurantsRepository, IMapper mapper) : IRequestHandler<UpdateRestaurantCommand, bool>
+    IRestaurantsRepository restaurantsRepository, IMapper mapper) : IRequestHandler<UpdateRestaurantCommand>
 {
-    public async Task<bool> Handle(UpdateRestaurantCommand request, CancellationToken cancellationToken)
+    public async Task Handle(UpdateRestaurantCommand request, CancellationToken cancellationToken)
     {
         logger.LogInformation("Updating restaurant with id: {RestaurantId} with {@UpdatedRestaurant}", request.Id, request);
         var restaurant = await restaurantsRepository.GetByIdAsync(request.Id)
@@ -23,7 +23,5 @@ public class UpdateRestaurantCommandHandler(ILogger<UpdateRestaurantCommandHandl
         mapper.Map(request, restaurant);
 
         await restaurantsRepository.SaveChangesAsync();
-
-        return true;
     }
 }
