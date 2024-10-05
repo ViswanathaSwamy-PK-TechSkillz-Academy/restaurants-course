ca4368cd652f5787ef67b6685f8a735c2e6342f9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-10-05 18:13:19 +0530

    Create DishesController.cs

---
+++ src/Restaurants.API/Controllers/DishesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurants.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DishesController : ControllerBase
+{
+}
