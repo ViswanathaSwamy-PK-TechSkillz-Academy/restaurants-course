a7806837a2d500964a333c7c109ba12ee103a755
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-10-07 08:57:54 +0530

    Swamy/05oct work (#13)
    * Create DishesController.cs
    
    * Update DishesController.cs
    
    * Create Dish
    
    * Dishes - 1
    
    * Changes

---
+++ src/Restaurants.API/Controllers/DishesController.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurants.API.Controllers;
+
+[Route("api/restaurants/{restaurantId}/[controller]")]
+[ApiController]
+public class DishesController(IMediator mediator) : ControllerBase
+{
+
+    [HttpPost]
+    public async Task<IActionResult> CreateDish([FromRoute] int restaurantId, CreateDishCommand command)
+    {
+        //command.RestaurantId = restaurantId;
+
+        await mediator.Send(command);
+
+        return Created();
+    }
+}
