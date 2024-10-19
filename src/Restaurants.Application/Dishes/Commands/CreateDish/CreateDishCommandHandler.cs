3e72be516f6da904424405ccd24ba44425e4db7f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-10-19 22:55:08 +0530

    Update CreateDishCommandHandler.cs

--- src/Restaurants.Application/Dishes/Commands/CreateDish/CreateDishCommandHandler.cs
+++ src/Restaurants.Application/Dishes/Commands/CreateDish/CreateDishCommandHandler.cs
@@ -9,18 +9,17 @@ namespace Restaurants.Application.Dishes.Commands.CreateDish;
 
 public class CreateDishCommandHandler(ILogger<CreateDishCommandHandler> logger,
     IRestaurantsRepository restaurantsRepository,
-    IDishesRepository dishesRepository,
     IMapper mapper) : IRequestHandler<CreateDishCommand>
 {
     public async Task Handle(CreateDishCommand request, CancellationToken cancellationToken)
     {
         logger.LogInformation("Creating new dish: {@DishRequest}", request);
-        var restaurant = await restaurantsRepository.GetByIdAsync(request.RestaurantId);
-        if (restaurant == null) throw new NotFoundException(nameof(Restaurant), request.RestaurantId.ToString());
+
+        Restaurant? restaurant = await restaurantsRepository.GetByIdAsync(request.RestaurantId) ?? throw new NotFoundException(nameof(Restaurant), request.RestaurantId.ToString());
 
         var dish = mapper.Map<Dish>(request);
 
-        await dishesRepository.Create(dish);
+        //await dishesRepository.Create(dish);
 
     }
 }
