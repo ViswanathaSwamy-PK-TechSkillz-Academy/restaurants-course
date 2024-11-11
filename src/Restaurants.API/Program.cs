ac1510d46ef6f2f2dbd9d60da6b055df65631688
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-11-11 22:20:55 +0530

    Migrations

--- src/Restaurants.API/Program.cs
+++ src/Restaurants.API/Program.cs
@@ -1,5 +1,6 @@
 using Restaurants.API.Extensions;
 using Restaurants.API.Middlewares;
+using Restaurants.Domain.Entities;
 using Restaurants.Infrastructure.Seeders;
 using Serilog;
 
@@ -32,6 +33,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapGroup("api/identity")
+       .WithTags("Identity")
+       .MapIdentityApi<User>();
 
 app.UseAuthorization();
 
