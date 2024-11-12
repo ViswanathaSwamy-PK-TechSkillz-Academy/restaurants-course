d102651e8d5d45e23892032fbd5ef4d68ca789be
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-11-12 18:34:42 +0530

    Update IdentityController.cs

--- src/Restaurants.API/Controllers/IdentityController.cs
+++ src/Restaurants.API/Controllers/IdentityController.cs
@@ -1,6 +1,8 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Restaurants.Application.Users.Commands.AssignUserRole;
+using Restaurants.Application.Users.Commands.UnassignUserRole;
 using Restaurants.Application.Users.Commands.UpdateUserDetails;
 
 namespace Restaurants.API.Controllers;
@@ -19,19 +21,21 @@ public class IdentityController(IMediator mediator) : ControllerBase
         return NoContent();
     }
 
-    //[HttpPost("userRole")]
-    ////[Authorize(Roles = UserRoles.Admin)]
-    //public async Task<IActionResult> AssignUserRole(AssignUserRoleCommand command)
-    //{
-    //    await mediator.Send(command);
-    //    return NoContent();
-    //}
+    [HttpPost("userRole")]
+    //[Authorize(Roles = UserRoles.Admin)]
+    public async Task<IActionResult> AssignUserRole(AssignUserRoleCommand command)
+    {
+        await mediator.Send(command);
 
-    //[HttpDelete("userRole")]
-    ////[Authorize(Roles = UserRoles.Admin)]
-    //public async Task<IActionResult> UnassignUserRole(UnassignUserRoleCommand command)
-    //{
-    //    await mediator.Send(command);
-    //    return NoContent();
-    //}
+        return NoContent();
+    }
+
+    [HttpDelete("userRole")]
+    //[Authorize(Roles = UserRoles.Admin)]
+    public async Task<IActionResult> UnassignUserRole(UnassignUserRoleCommand command)
+    {
+        await mediator.Send(command);
+
+        return NoContent();
+    }
 }
