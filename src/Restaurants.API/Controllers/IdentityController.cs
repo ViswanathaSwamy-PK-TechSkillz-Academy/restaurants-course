d204ae8fa7614096cd3d46f4e4dca98be6e133e3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-11-14 12:47:09 +0530

    Changes

--- src/Restaurants.API/Controllers/IdentityController.cs
+++ src/Restaurants.API/Controllers/IdentityController.cs
@@ -1,7 +1,9 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Restaurants.Application.Users.Commands.AssignUserRole;
 using Restaurants.Application.Users.Commands.UpdateUserDetails;
+using Restaurants.Domain.Constants;
 
 namespace Restaurants.API.Controllers;
 
@@ -19,14 +21,14 @@ public class IdentityController(IMediator mediator) : ControllerBase
         return NoContent();
     }
 
-    //[HttpPost("userRole")]
-    ////[Authorize(Roles = UserRoles.Admin)]
-    //public async Task<IActionResult> AssignUserRole(AssignUserRoleCommand command)
-    //{
-    //    await mediator.Send(command);
+    [HttpPost("userRole")]
+    [Authorize(Roles = UserRoles.Admin)]
+    public async Task<IActionResult> AssignUserRole(AssignUserRoleCommand command)
+    {
+        await mediator.Send(command);
 
-    //    return NoContent();
-    //}
+        return NoContent();
+    }
 
     //[HttpDelete("userRole")]
     ////[Authorize(Roles = UserRoles.Admin)]
