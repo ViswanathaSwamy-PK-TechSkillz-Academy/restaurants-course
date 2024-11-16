2f3cede9a124d1fb1f14ee1e25a99a6e0c75965d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-11-16 13:00:50 +0530

    Swamy/15nov work (#20)
    * Changes
    
    * Changes
    
    * Update Constants.cs

--- src/Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
 using Restaurants.Infrastructure.Authorization;
+using Restaurants.Infrastructure.Authorization.Requirements;
 using Restaurants.Infrastructure.Persistence;
 using Restaurants.Infrastructure.Repositories;
 using Restaurants.Infrastructure.Seeders;
@@ -34,6 +35,14 @@ public static class ServiceCollectionExtensions
         services.AddScoped<IRestaurantsRepository, RestaurantsRepository>();
 
         services.AddScoped<IDishesRepository, DishesRepository>();
+
+        services.AddAuthorizationBuilder()
+            .AddPolicy(PolicyNames.HasNationality,
+                builder => builder.RequireClaim(AppClaimTypes.Nationality, "German", "Polish"))
+            .AddPolicy(PolicyNames.AtLeast20,
+                builder => builder.AddRequirements(new MinimumAgeRequirement(20)))
+            .AddPolicy(PolicyNames.CreatedAtleast2Restaurants,
+                builder => builder.AddRequirements(new CreatedMultipleRestaurantsRequirement(2)));
     }
 
 }
