b58c6673f86d61247ab428c7efb30f1db2224c20
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-11-18 16:18:01 +0530

    Update DeleteDishesForRestaurantCommandHandler.cs

--- src/Restaurants.Application/Dishes/Commands/DeleteDishes/DeleteDishesForRestaurantCommandHandler.cs
+++ src/Restaurants.Application/Dishes/Commands/DeleteDishes/DeleteDishesForRestaurantCommandHandler.cs
@@ -1,13 +1,15 @@
 ﻿using MediatR;
 using Microsoft.Extensions.Logging;
+using Restaurants.Domain.Constants;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Exceptions;
+using Restaurants.Domain.Interfaces;
 using Restaurants.Domain.Repositories;
 
 namespace Restaurants.Application.Dishes.Commands.DeleteDishes;
 
 public class DeleteDishesForRestaurantCommandHandler(ILogger<DeleteDishesForRestaurantCommandHandler> logger,
-    IRestaurantsRepository restaurantsRepository, IDishesRepository dishesRepository) :
+    IRestaurantsRepository restaurantsRepository, IDishesRepository dishesRepository, IRestaurantAuthorizationService restaurantAuthorizationService) :
     IRequestHandler<DeleteDishesForRestaurantCommand>
 {
     public async Task Handle(DeleteDishesForRestaurantCommand request, CancellationToken cancellationToken)
@@ -15,6 +17,9 @@ public class DeleteDishesForRestaurantCommandHandler(ILogger<DeleteDishesForRest
         logger.LogWarning("Removing all dishes from restaurant: {RestaurantId}", request.RestaurantId);
 
         var restaurant = await restaurantsRepository.GetByIdAsync(request.RestaurantId) ?? throw new NotFoundException(nameof(Restaurant), request.RestaurantId.ToString());
+
+        if (!restaurantAuthorizationService.Authorize(restaurant, ResourceOperation.Update))
+            throw new ForbidException();
 
         await dishesRepository.Delete(restaurant.Dishes);
     }
