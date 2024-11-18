5eadcac185752b2aaa2ebfb6e865e0f30cccdac7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-11-18 16:02:46 +0530

    Update ServiceCollectionExtensions.cs

--- src/Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -4,9 +4,11 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Restaurants.Domain.Entities;
+using Restaurants.Domain.Interfaces;
 using Restaurants.Domain.Repositories;
 using Restaurants.Infrastructure.Authorization;
 using Restaurants.Infrastructure.Authorization.Requirements;
+using Restaurants.Infrastructure.Authorization.Services;
 using Restaurants.Infrastructure.Persistence;
 using Restaurants.Infrastructure.Repositories;
 using Restaurants.Infrastructure.Seeders;
@@ -46,6 +48,8 @@ public static class ServiceCollectionExtensions
         //    builder => builder.AddRequirements(new CreatedMultipleRestaurantsRequirement(2)));
 
         services.AddScoped<IAuthorizationHandler, MinimumAgeRequirementHandler>();
+        
+        services.AddScoped<IRestaurantAuthorizationService, RestaurantAuthorizationService>();
     }
 
 }
