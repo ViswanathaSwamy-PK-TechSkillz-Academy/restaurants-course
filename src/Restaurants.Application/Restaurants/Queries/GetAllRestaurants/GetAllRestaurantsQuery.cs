facfa853d2911a09301866bdead4946115a4e720
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-11-21 14:39:08 +0530

    Update GetAllRestaurantsQuery.cs

--- src/Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQuery.cs
+++ src/Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQuery.cs
@@ -5,4 +5,10 @@ namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
 
 public class GetAllRestaurantsQuery : IRequest<IEnumerable<RestaurantDto>>
 {
+    public string? SearchPhrase { get; set; }
+
+    //public int PageNumber { get; set; }
+    //public int PageSize { get; set; }
+    //public string? SortBy { get; set; }
+    //public SortDirection SortDirection { get; set; }
 }
