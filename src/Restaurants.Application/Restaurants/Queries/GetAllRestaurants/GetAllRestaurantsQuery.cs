359e1aa88319f3a31f875bbc8e77b824de000993
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-11-22 19:05:57 +0530

    Update GetAllRestaurantsQuery.cs

--- src/Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQuery.cs
+++ src/Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQuery.cs
@@ -7,8 +7,9 @@ public class GetAllRestaurantsQuery : IRequest<IEnumerable<RestaurantDto>>
 {
     public string? SearchPhrase { get; set; }
 
-    //public int PageNumber { get; set; }
-    //public int PageSize { get; set; }
+    public int PageNumber { get; set; }
+
+    public int PageSize { get; set; }
     //public string? SortBy { get; set; }
     //public SortDirection SortDirection { get; set; }
 }
