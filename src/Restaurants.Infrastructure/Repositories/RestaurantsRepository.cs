a3f7511d2c305d38d2c5b73ad6a8285ad8bfb9fe
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-11-22 21:07:34 +0530

    Update RestaurantsRepository.cs

--- src/Restaurants.Infrastructure/Repositories/RestaurantsRepository.cs
+++ src/Restaurants.Infrastructure/Repositories/RestaurantsRepository.cs
@@ -3,6 +3,7 @@ using Restaurants.Domain.Constants;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
 using Restaurants.Infrastructure.Persistence;
+using System.Linq.Expressions;
 
 namespace Restaurants.Infrastructure.Repositories;
 
@@ -25,6 +26,22 @@ internal class RestaurantsRepository(RestaurantsDbContext dbContext) : IRestaura
                         (r.Name.ToLower().Contains(searchPhraseLower) || r.Description.ToLower().Contains(searchPhraseLower)));
 
         var totalCount = await baseQuery.CountAsync();
+
+        if (sortBy != null)
+        {
+            var columnsSelector = new Dictionary<string, Expression<Func<Restaurant, object>>>
+            {
+                { nameof(Restaurant.Name), r => r.Name },
+                { nameof(Restaurant.Description), r => r.Description },
+                { nameof(Restaurant.Category), r => r.Category },
+            };
+
+            var selectedColumn = columnsSelector[sortBy];
+
+            baseQuery = sortDirection == SortDirection.Ascending
+                ? baseQuery.OrderBy(selectedColumn)
+                : baseQuery.OrderByDescending(selectedColumn);
+        }
 
         var restaurants = await baseQuery
             .Skip(pageSize * (pageNumber - 1))
