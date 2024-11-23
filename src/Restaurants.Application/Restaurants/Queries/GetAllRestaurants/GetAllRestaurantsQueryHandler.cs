464fae6b5376b6ed2a53eb1d6494f717783866c2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-11-23 19:06:31 +0530

    Swamy/22nov work (#26)
    * Changes
    
    * Update RestaurantsRepository.cs
    
    * Update GetAllRestaurantsQuery.cs
    
    * Page Size, and Page Number
    
    * Changes
    
    * Update GetAllRestaurantsQueryHandler.cs
    
    * Update Restaurants.API.http
    
    * Update Restaurants.API.http
    
    * Update Restaurants.API.http
    
    * Update GetAllRestaurantsQueryValidator.cs
    
    * Sort - 1
    
    * Update GetAllRestaurantsQueryValidator.cs
    
    * Added Sort Parameters
    
    * Update RestaurantsRepository.cs
    
    * Update Restaurants.API.http

--- src/Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQueryHandler.cs
+++ src/Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQueryHandler.cs
@@ -1,23 +1,26 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using Restaurants.Application.Common;
 using Restaurants.Application.Restaurants.Dtos;
 using Restaurants.Domain.Repositories;
 
 namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
 
 public class GetAllRestaurantsQueryHandler(ILogger<GetAllRestaurantsQueryHandler> logger,
-    IMapper mapper, IRestaurantsRepository restaurantsRepository) : IRequestHandler<GetAllRestaurantsQuery, IEnumerable<RestaurantDto>>
+    IMapper mapper, IRestaurantsRepository restaurantsRepository) : IRequestHandler<GetAllRestaurantsQuery, PagedResult<RestaurantDto>>
 {
-    public async Task<IEnumerable<RestaurantDto>> Handle(GetAllRestaurantsQuery request, CancellationToken cancellationToken)
+    public async Task<PagedResult<RestaurantDto>> Handle(GetAllRestaurantsQuery request, CancellationToken cancellationToken)
     {
         logger.LogInformation("Getting all restaurants");
 
-        //var restaurants = await restaurantsRepository.GetAllAsync();
-        var restaurants = await restaurantsRepository.GetAllMatchingAsync(request.SearchPhrase);
+        var (restaurants, totalCount) = await restaurantsRepository.GetAllMatchingAsync(request.SearchPhrase,
+            request.PageSize, request.PageNumber, request.SortBy, request.SortDirection);
 
         var restaurantsDto = mapper.Map<IEnumerable<RestaurantDto>>(restaurants);
 
-        return restaurantsDto!; // It will not be null. It might be empty collection.
+        var result = new PagedResult<RestaurantDto>(restaurantsDto, totalCount, request.PageSize, request.PageNumber);
+
+        return result; // It will not be null. It might be empty collection.
     }
 }
--- src/Restaurants.Infrastructure/Repositories/RestaurantsRepository.cs
+++ src/Restaurants.Infrastructure/Repositories/RestaurantsRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Constants;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
 using Restaurants.Infrastructure.Persistence;
@@ -15,41 +16,39 @@ internal class RestaurantsRepository(RestaurantsDbContext dbContext) : IRestaura
         return restaurants;
     }
 
-    public async Task<IEnumerable<Restaurant>> GetAllMatchingAsync(string? searchPhrase)
+    public async Task<(IEnumerable<Restaurant>, int)> GetAllMatchingAsync(string? searchPhrase, int pageSize, int pageNumber, string? sortBy, SortDirection sortDirection)
     {
         var searchPhraseLower = searchPhrase?.ToLower();
 
-        var restaurants = await dbContext
+        var baseQuery = dbContext
             .Restaurants
             .Where(r => searchPhraseLower == null ||
-                   (r.Name.ToLower().Contains(searchPhraseLower) ||
-                   r.Description.ToLower().Contains(searchPhraseLower)))
+                        (r.Name.ToLower().Contains(searchPhraseLower) || r.Description.ToLower().Contains(searchPhraseLower)));
+
+        var totalCount = await baseQuery.CountAsync();
+
+        if (sortBy != null)
+        {
+            var columnsSelector = new Dictionary<string, Expression<Func<Restaurant, object>>>
+            {
+                { nameof(Restaurant.Name), r => r.Name },
+                { nameof(Restaurant.Description), r => r.Description },
+                { nameof(Restaurant.Category), r => r.Category },
+            };
+
+            var selectedColumn = columnsSelector[sortBy];
+
+            baseQuery = sortDirection == SortDirection.Ascending
+                ? baseQuery.OrderBy(selectedColumn)
+                : baseQuery.OrderByDescending(selectedColumn);
+        }
+
+        var restaurants = await baseQuery
+            .Skip(pageSize * (pageNumber - 1))
+            .Take(pageSize)
             .ToListAsync();
 
-        //var totalCount = await restaurants.CountAsync();
-
-        //if (sortBy != null)
-        //{
-        //    var columnsSelector = new Dictionary<string, Expression<Func<Restaurant, object>>>
-        //    {
-        //        { nameof(Restaurant.Name), r => r.Name },
-        //        { nameof(Restaurant.Description), r => r.Description },
-        //        { nameof(Restaurant.Category), r => r.Category },
-        //    };
-
-        //    var selectedColumn = columnsSelector[sortBy];
-
-        //    baseQuery = sortDirection == SortDirection.Ascending
-        //        ? baseQuery.OrderBy(selectedColumn)
-        //        : baseQuery.OrderByDescending(selectedColumn);
-        //}
-
-        //var restaurants = await baseQuery
-        //    .Skip(pageSize * (pageNumber - 1))
-        //    .Take(pageSize)
-        //    .ToListAsync();
-
-        return restaurants;
+        return (restaurants, totalCount);
     }
 
     public async Task<Restaurant?> GetByIdAsync(int id)
