02abf2dc7ed20febb9d562c3c352e96432fa52a9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-11-23 21:54:38 +0530

    Update Program.cs

--- src/Restaurants.API/Program.cs
+++ src/Restaurants.API/Program.cs
@@ -4,40 +4,52 @@ using Restaurants.Domain.Entities;
 using Restaurants.Infrastructure.Seeders;
 using Serilog;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.AddPresentation();
-
-WebApplication app = builder.Build();
-
-app.UseMiddleware<ErrorHandlingMiddleware>();
-
-app.UseMiddleware<RequestTimeLoggingMiddleware>();
-
-app.UseSerilogRequestLogging();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
 
-    // Seeding the database
-    var scope = app.Services.CreateScope();
-    var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
+    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-    await seeder.Seed();
+    // Add services to the container.
+    builder.AddPresentation();
+
+    WebApplication app = builder.Build();
+
+    app.UseMiddleware<ErrorHandlingMiddleware>();
+
+    app.UseMiddleware<RequestTimeLoggingMiddleware>();
+
+    app.UseSerilogRequestLogging();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        // Seeding the database
+        var scope = app.Services.CreateScope();
+        var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
+
+        await seeder.Seed();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.MapGroup("api/identity")
+           .WithTags("Identity")
+           .MapIdentityApi<User>();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.MapGroup("api/identity")
-       .WithTags("Identity")
-       .MapIdentityApi<User>();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application startup failed");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
