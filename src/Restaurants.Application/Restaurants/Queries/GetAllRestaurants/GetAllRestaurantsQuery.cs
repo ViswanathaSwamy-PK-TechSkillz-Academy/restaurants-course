b839419b0efaa96145f5945df67fda88b29e3f4d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-11-25 07:26:55 +0530

    Swamy/24nov work (#28)
    * Removed Test Project
    
    * Update Restaurants.API.csproj
    
    * Application Insights
    
    * Update GetAllRestaurantsQuery.cs

--- src/Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQuery.cs
+++ src/Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQuery.cs
@@ -9,9 +9,9 @@ public class GetAllRestaurantsQuery : IRequest<PagedResult<RestaurantDto>>
 {
     public string? SearchPhrase { get; set; }
 
-    public int PageNumber { get; set; }
+    public int PageNumber { get; set; } = 1;
 
-    public int PageSize { get; set; }
+    public int PageSize { get; set; } = 5;
 
     public string? SortBy { get; set; }
 
