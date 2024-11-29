e475ec315dc9165725f13511b0cc8983fc31824c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-11-29 09:16:19 +0530

    Swamy/26nov work (#30)
    * Update Restaurants.API.csproj
    
    * Blob Storage
    
    * Update RestaurantsController.cs
    
    * Update Restaurants.Infrastructure.csproj
    
    * Changes

---
+++ src/Restaurants.Application/Restaurants/Commands/UploadRestaurantLogo/UploadRestaurantLogoCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Restaurants.Application.Restaurants.Commands.UploadRestaurantLogo;
+
+public class UploadRestaurantLogoCommand : IRequest
+{
+    public int RestaurantId { get; set; }
+
+    public string FileName { get; set; } = string.Empty;
+
+    public Stream File { get; set; } = default!;
+}
