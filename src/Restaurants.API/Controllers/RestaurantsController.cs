cd7d2654d098109a5a56a5460d7deaa4f1d68fa6
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-12-02 17:40:27 +0530

    Changes

--- src/Restaurants.API/Controllers/RestaurantsController.cs
+++ src/Restaurants.API/Controllers/RestaurantsController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Restaurants.Application.Restaurants.Commands.CreateRestaurant;
 using Restaurants.Application.Restaurants.Commands.DeleteRestaurant;
 using Restaurants.Application.Restaurants.Commands.UpdateRestaurant;
+using Restaurants.Application.Restaurants.Commands.UploadRestaurantLogo;
 using Restaurants.Application.Restaurants.Dtos;
 using Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
 using Restaurants.Application.Restaurants.Queries.GetRestaurantById;
@@ -89,6 +90,7 @@ public class RestaurantsController(IMediator mediator) : ControllerBase
         };
 
         await mediator.Send(command);
+
         return NoContent();
     }
 }
--- src/Restaurants.Application/Restaurants/Commands/UploadRestaurantLogo/UploadRestaurantLogoCommandHandler.cs
+++ src/Restaurants.Application/Restaurants/Commands/UploadRestaurantLogo/UploadRestaurantLogoCommandHandler.cs
@@ -17,9 +17,8 @@ internal class UploadRestaurantLogoCommandHandler(ILogger<UploadRestaurantLogoCo
     public async Task Handle(UploadRestaurantLogoCommand request, CancellationToken cancellationToken)
     {
         logger.LogInformation("Uploading restaurant logo for id: {RestaurantId}", request.RestaurantId);
-        var restaurant = await restaurantsRepository.GetByIdAsync(request.RestaurantId);
-        if (restaurant is null)
-            throw new NotFoundException(nameof(Restaurant), request.RestaurantId.ToString());
+
+        var restaurant = await restaurantsRepository.GetByIdAsync(request.RestaurantId) ?? throw new NotFoundException(nameof(Restaurant), request.RestaurantId.ToString());
 
         if (!restaurantAuthorizationService.Authorize(restaurant, ResourceOperation.Update))
             throw new ForbidException();
