85269e130beb3762971469558a9115568d05eecd
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-12-02 17:41:48 +0530

    Create BlobStorageService.cs

---
+++ src/Restaurants.Infrastructure/Storage/BlobStorageService.cs
@@ -0,0 +1,11 @@
+﻿using Restaurants.Domain.Interfaces;
+
+namespace Restaurants.Infrastructure.Storage;
+
+internal class BlobStorageService : IBlobStorageService
+{
+    public Task<string> UploadToBlobAsync(Stream data, string fileName)
+    {
+        throw new NotImplementedException();
+    }
+}
