02334ce4f1415c181a7beb65e8da573840013239
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-12-04 18:46:13 +0530

    Swamy/02dec work (#32)
    * Changes
    
    * Create BlobStorageService.cs
    
    * Update BlobStorageService.cs
    
    * Create BlobStorageSettings.cs
    
    * Update BlobStorageService.cs
    
    * Changes
    
    * Added Logo Url
    
    * Added New Migrations
    
    * Update README.md
    
    * Update Restaurants.API.http

--- src/Restaurants.Application/Restaurants/Commands/UploadRestaurantLogo/UploadRestaurantLogoCommandHandler.cs
+++ src/Restaurants.Application/Restaurants/Commands/UploadRestaurantLogo/UploadRestaurantLogoCommandHandler.cs
@@ -17,9 +17,8 @@ internal class UploadRestaurantLogoCommandHandler(ILogger<UploadRestaurantLogoCo
     public async Task Handle(UploadRestaurantLogoCommand request, CancellationToken cancellationToken)
     {
         logger.LogInformation("Uploading restaurant logo for id: {RestaurantId}", request.RestaurantId);
-        var restaurant = await restaurantsRepository.GetByIdAsync(request.RestaurantId);
-        if (restaurant is null)
-            throw new NotFoundException(nameof(Restaurant), request.RestaurantId.ToString());
+
+        var restaurant = await restaurantsRepository.GetByIdAsync(request.RestaurantId) ?? throw new NotFoundException(nameof(Restaurant), request.RestaurantId.ToString());
 
         if (!restaurantAuthorizationService.Authorize(restaurant, ResourceOperation.Update))
             throw new ForbidException();
@@ -27,6 +26,6 @@ internal class UploadRestaurantLogoCommandHandler(ILogger<UploadRestaurantLogoCo
         var logoUrl = await blobStorageService.UploadToBlobAsync(request.File, request.FileName);
         restaurant.LogoUrl = logoUrl;
 
-        await restaurantsRepository.SaveChanges();
+        await restaurantsRepository.SaveChangesAsync();
     }
 }
---
+++ src/Restaurants.Infrastructure/Configuration/BlobStorageSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Restaurants.Infrastructure.Configuration;
+
+public class BlobStorageSettings
+{
+    public string ConnectionString { get; set; } = string.Empty;
+
+    public string LogosContainerName { get; set; } = string.Empty;
+
+    public string AccountKey { get; set; } = string.Empty;
+}
